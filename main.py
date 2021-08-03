d481471e47971e381f4a9f776f238f7fe92f83b6
Hazim Avdal <dev@avd.al>
2021-08-02 22:53:12 -0500

    changed log message

--- main.py
+++ main.py
@@ -383,7 +383,11 @@ if __name__ == "__main__":
         manf(repos, args.func, app, logger, errors)
 
         sys.exit(errors)
-        logger.info(f"Finished with {errors} errors")
+
+        count = 'no' if errors == 0 else str(errors)
+        plural = '' if errors == 1 else 's'
+
+        logger.info(msg=f"Finished with {count} error{plural}")
     except Exception as err:
         logger.error(err)
         sys.exit(1)
